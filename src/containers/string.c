94a90deee83b10d3490a40bfa41073b7cbb685cb
GeorgeGabor96 <gaborgeorge96@gmail.com>
2022-09-19 22:21:06 +0300

    Initial string implementation need to test and figure out the OS problem

---
+++ src/containers/string.c
@@ -0,0 +1,108 @@
+#include "containers/string.h"
+
+
+inline internal u32
+get_c_str_length(const char* c_str) {
+    u32 length = 0;
+    while (*c_str) {
+        ++length;
+        ++c_str;
+    }
+    return length;
+}
+
+#define str_size(length) (sizeof(String) + length + 1)
+
+inline internal String*
+string_create_from_length(u32 length) {
+    String* str = (String*) memory_malloc(str_size(length),
+                                          "string_create_from_length");
+    check_memory(str);
+    str->length = length;
+    str->data = str + 1;
+    error:
+    return str;
+}
+
+internal String*
+string_create(const char* c_str) {
+    String* str = NULL;
+    check(c_str != NULL, "c_str is NULL");
+    
+    u32 length = get_c_str_length(c_str);
+    str = string_create_from_length(length);
+    check_memory(str);
+    memcpy(str->data, c_str, str->length + 1);
+    
+    error:
+    return str;
+}
+
+
+internal void
+string_destroy(String* str) {
+    check(str != NULL, "str is NULL");
+    memset(str, 0, str_size(str));
+    memory_free(str);
+    
+    error:
+    return;
+}
+
+
+internal String*
+string_path_join(String* str1, String* str2, bool keep_strs) {
+    String* str = NULL;
+    check(str1 != NULL, "str1 is NULL");
+    check(str2 != NULL, "str2 is NULL");
+    
+    u32 length = str1->length + 1 + str2->length;
+    str = string_create_from_length(length);
+    u32 str_offset = 0;
+    u32 i = 0;
+    for (i = 0; i < str1->length; ++i, ++str_offset) {
+        str->data[str_offset] = str1->data[i];
+    }
+    str->data[str_offset++] = STR_OS_PATH_SEP;
+    for (i = 0; i < str2->length; ++i, ++str_offset) {
+        str->data[str_offset] = str2->data[i];
+    } 
+    str->data[str_offset] = '\0';
+    
+    if (keep_strs == FALSE) {
+        string_destroy(str1);
+        string_destroy(str2);
+    }
+    
+    error:
+    return str;
+}
+
+
+internal const char*
+string_to_c_str(String* str) {
+    check(str != NULL, "str is NULL");
+    
+    return (const char*)str->data;
+    
+    error:
+    return str;
+}
+
+
+internal char*
+string_to_c_str_copy(String* str) {
+    check(str != NULL, "str is NULL");
+    
+    char* c_str = (char*) memory_malloc((str->length + 1) * sizeof(char));
+    check_memory(c_str);
+    
+    u32 i = 0;
+    for (i = 0; i < str->length; ++i) {
+        c_str[i] = str->data[i];
+    }
+    c_str[i] = '\0';
+    
+    error:
+    return c_str;
+}
--- src/containers/string.h
+++ src/containers/string.h
@@ -6,9 +6,22 @@
 #include "common.h"
 
 
+// TODO: need to know which os sep to use, how do I know I'm on windows or linux???
+//#define STR_OS_PATH_SEP '/'
+#define STR_OS_PATH_SEP '\\'
+
+
 typedef struct String {
     u32 length;
-    u8* data;   // NOTE: this should point directly after the String
+    char* data;   // NOTE: this should point directly after the String
 } String;
+
+
+internal String* string_create(const char* c_str);
+internal void* string_destroy(String* str);
+internal String* string_path_join(String* str1, String* str2, bool keep_strs);
+
+internal const char* string_to_c_str(String* str);
+internal char* string_to_c_str_copy(String* str);
 
 #endif //STRING_H
