80f42112392d36e442878d4c01dc46d56d1c9d35
GeorgeGabor96 <gaborgeorge96@yahoo.com>
2022-10-23 15:34:33 +0300

    Added functions for working with directories in windows

---
+++ src/utils/os.h
@@ -0,0 +1,25 @@
+/* date = October 23rd 2022 1:52 pm */
+
+#ifndef OS_H
+#define OS_H
+
+#include "common.h"
+#include "containers/string.h"
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+#include <Windows.h>
+
+internal bool os_folder_create_c_str(const char* path);
+internal bool os_folder_create_str(String* path);
+
+internal bool os_folder_delete_c_str(const char* path);
+internal bool os_folder_delete_str(String* path);
+
+internal bool os_folder_exists_c_str(const char* path);
+internal bool os_folder_exists_str(String* path);
+
+
+#endif //OS_H
