43a894f4ddfebfd428780b921678ba1d901aff18
GeorgeGabor96 <gaborgeorge96@yahoo.com>
2023-01-05 16:44:20 +0200

    Added cmd arguments

--- go/main.go
+++ go/main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
+	"os"
 	"tango/go/network"
 	"tango/go/utils"
 )
 
+type Arguments struct {
+	binFolder string
+}
+
+func ParseArguments() (*Arguments, error) {
+	args := new(Arguments)
+
+	for i := 1; i < len(os.Args); i++ {
+		if os.Args[i] == "--bin_folder" {
+			i++
+			args.binFolder = os.Args[i]
+		} else {
+			return nil, errors.New(fmt.Sprintf("Unknown Argument %v", os.Args[i]))
+		}
+	}
+
+	return args, nil
+}
+
 func main() {
-	dir := "d:\\repos\\Tango_outputs\\Threads\\debug\\"
-	sampleNames := utils.FileNamesWithExtension(dir, "bin")
+	args, err := ParseArguments()
+	if err != nil {
+		panic(err)
+	}
+	sampleNames := utils.FileNamesWithExtension(args.binFolder, "bin")
 	for _, sampleName := range sampleNames {
-		net, err := network.NewNetworkSample(utils.Join(dir, sampleName))
+		net, err := network.NewNetworkSample(utils.Join(args.binFolder, sampleName))
 		if err != nil {
 			log.Fatal(err)
 		}
