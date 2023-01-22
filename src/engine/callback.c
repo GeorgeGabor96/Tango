5823c6ca27745462a96995c7939c45152b564983
GeorgeGabor96 <gaborgeorge96@gmail.com>
2023-01-22 19:54:17 +0200

    removed an include

--- src/engine/callback.c
+++ src/engine/callback.c
@@ -1,6 +1,3 @@
-#include "engine/callbacks/dumper.c"
-
-
 internal char*
 callback_type_get_c_str(CallbackType type) {
     if (type == CALLBACK_NETWORK_DUMPER) return "CALLBACK_NETWORK_DUMPER";
@@ -17,7 +14,7 @@ callback_begin_sample(State* state,
     check(callback != NULL, "callback is NULL");
     check(network != NULL, "network is NULL");
     check(sample_duration != 0, "sample_duration is NULL");
-    
+
     if (callback->type == CALLBACK_NETWORK_DUMPER) {
         callback_dumper_begin_sample(state, callback, network, sample_duration);
     } else {
@@ -25,7 +22,7 @@ callback_begin_sample(State* state,
                   callback->type,
                   callback_type_get_c_str(callback->type));
     }
-    
+
     error:
     return;
 }
@@ -36,7 +33,7 @@ callback_update(State* state, Callback* callback, Network* network) {
     check(state != NULL, "state is NULL");
     check(callback != NULL, "callback is NULL");
     check(network != NULL, "network is NULL");
-    
+
     if (callback->type == CALLBACK_NETWORK_DUMPER) {
         callback_dumper_update(state, callback, network);
     } else {
@@ -44,7 +41,7 @@ callback_update(State* state, Callback* callback, Network* network) {
                   callback->type,
                   callback_type_get_c_str(callback->type));
     }
-    
+
     error:
     return;
 }
@@ -55,7 +52,7 @@ callback_end_sample(State* state, Callback* callback, Network* network) {
     check(state != NULL, "state si NULL");
     check(callback != NULL, "callback is NULL");
     check(network != NULL, "network is NULL");
-    
+
     if (callback->type == CALLBACK_NETWORK_DUMPER) {
         callback_dumper_end_sample(state, callback, network);
     } else {
@@ -63,7 +60,7 @@ callback_end_sample(State* state, Callback* callback, Network* network) {
                   callback->type,
                   callback_type_get_c_str(callback->type));
     }
-    
+
     error:
     return;
 }
