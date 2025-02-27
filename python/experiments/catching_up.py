15848a9aaf5d6a0d9307abb515932be92f7c815c
GeorgeGabor96 <gaborgeorge96@gmail.com>
2025-02-27 22:07:16 +0200

    Added network that can perform xor and nxor, was build by hand

--- python/experiments/catching_up.py
+++ python/experiments/catching_up.py
@@ -163,8 +163,8 @@ def test_xor():
     synapse_in_1_hidden_2 = Synapse(w = -1)
     synapse_in_2_hidden_2 = Synapse(w = 1)
 
-    neuron_hidden_1 = Neuron()
-    neuron_hidden_2 = Neuron()
+    neuron_hidden_1 = Neuron() # detects that neuron 1 is firing and neuron 2 is not
+    neuron_hidden_2 = Neuron() # detects that neuron 2 is firing and neuron 1 is not
 
     neuron_in_1.add_out_synapse(synapse_in_1_hidden_1)
     neuron_in_1.add_out_synapse(synapse_in_1_hidden_2)
@@ -179,9 +179,25 @@ def test_xor():
     neuron_hidden_2.add_in_synapse(synapse_in_2_hidden_2)
 
     # hidden layer neg
-    neuron_hidden_3 = Neuron()
-    synapse_in_3_hidden_3 = Synapse(0)
-    synapse_in_4_hidden_3 = Synapse(0)
+    neuron_hidden_3 = Neuron() # detects that neuron 1 is firing and neuron 2 is firing
+    synapse_in_1_hidden_3 = Synapse(0.5)
+    synapse_in_2_hidden_3 = Synapse(0.5)
+
+    neuron_hidden_3.add_in_synapse(synapse_in_1_hidden_3)
+    neuron_hidden_3.add_in_synapse(synapse_in_2_hidden_3)
+
+    neuron_in_1.add_out_synapse(synapse_in_1_hidden_3)
+    neuron_in_2.add_out_synapse(synapse_in_2_hidden_3)
+
+    neuron_hidden_4 = Neuron() # detects that neuron 1 is not firing and neuron 2 is not firing using neuron_in_3 and neuron_in_4 that spike when in1 and in2 don't
+    synapse_in_3_hidden_4 = Synapse(0.5)
+    synapse_in_4_hidden_4 = Synapse(0.5)
+
+    neuron_hidden_4.add_in_synapse(synapse_in_3_hidden_4)
+    neuron_hidden_4.add_in_synapse(synapse_in_4_hidden_4)
+
+    neuron_in_3.add_out_synapse(synapse_in_3_hidden_4)
+    neuron_in_4.add_out_synapse(synapse_in_4_hidden_4)
 
     # Output Layer
     synapse_hidden_1_output_xor = Synapse(w = 1)
@@ -194,6 +210,16 @@ def test_xor():
     neuron_output_xor.add_in_synapse(synapse_hidden_1_output_xor)
     neuron_output_xor.add_in_synapse(synapse_hidden_2_output_xor)
 
+    synapse_hidden_3_output_nxor = Synapse(w = 1)
+    synapse_hidden_4_output_nxor = Synapse(w = 1)
+    neuron_output_nxor = Neuron()
+
+    neuron_hidden_3.add_out_synapse(synapse_hidden_3_output_nxor)
+    neuron_hidden_4.add_out_synapse(synapse_hidden_4_output_nxor)
+
+    neuron_output_nxor.add_in_synapse(synapse_hidden_3_output_nxor)
+    neuron_output_nxor.add_in_synapse(synapse_hidden_4_output_nxor)
+
     for time in range(100):
         neuron_in_1_inject_current = 1 if (25 <= time < 50 or 75 <= time < 100) and time % 3 == 0 else 0
         neuron_in_1.inject_current(neuron_in_1_inject_current)
@@ -203,18 +229,34 @@ def test_xor():
         neuron_in_2.inject_current(neuron_in_2_inject_current)
         neuron_in_2.update(time)
 
+        neuron_in_3_inject_current = 1 if (time - neuron_in_1.last_spike_time) and time % 3 == 0 else 0
+        neuron_in_3.inject_current(neuron_in_3_inject_current)
+        neuron_in_3.update(time)
+
+        neuron_in_4_inject_current = 1 if (time - neuron_in_2.last_spike_time) and time % 3 == 0 else 0
+        neuron_in_4.inject_current(neuron_in_4_inject_current)
+        neuron_in_4.update(time)
+
         neuron_hidden_1.update(time)
         neuron_hidden_2.update(time)
+        neuron_hidden_3.update(time)
+        neuron_hidden_4.update(time)
 
         neuron_output_xor.update(time)
+        neuron_output_nxor.update(time)
 
-    _, ax = plt.subplots(5, sharex='all')
+    _, ax = plt.subplots(10, sharex='all')
     plt.xlim(0, 100)
     ax[0].scatter(neuron_in_1.spike_times, get_spikes_for_times(neuron_in_1.spike_times), marker='|')
     ax[1].scatter(neuron_in_2.spike_times, get_spikes_for_times(neuron_in_2.spike_times), marker='|')
-    ax[2].scatter(neuron_hidden_1.spike_times, get_spikes_for_times(neuron_hidden_1.spike_times), marker='|')
-    ax[3].scatter(neuron_hidden_2.spike_times, get_spikes_for_times(neuron_hidden_2.spike_times), marker='|')
-    ax[4].scatter(neuron_output_xor.spike_times, get_spikes_for_times(neuron_output_xor.spike_times), marker='|')
+    ax[2].scatter(neuron_in_3.spike_times, get_spikes_for_times(neuron_in_3.spike_times), marker='|')
+    ax[3].scatter(neuron_in_4.spike_times, get_spikes_for_times(neuron_in_4.spike_times), marker='|')
+    ax[4].scatter(neuron_hidden_1.spike_times, get_spikes_for_times(neuron_hidden_1.spike_times), marker='|')
+    ax[5].scatter(neuron_hidden_2.spike_times, get_spikes_for_times(neuron_hidden_2.spike_times), marker='|')
+    ax[6].scatter(neuron_hidden_3.spike_times, get_spikes_for_times(neuron_hidden_3.spike_times), marker='|')
+    ax[7].scatter(neuron_hidden_4.spike_times, get_spikes_for_times(neuron_hidden_4.spike_times), marker='|')
+    ax[8].scatter(neuron_output_xor.spike_times, get_spikes_for_times(neuron_output_xor.spike_times), marker='|')
+    ax[9].scatter(neuron_output_nxor.spike_times, get_spikes_for_times(neuron_output_nxor.spike_times), marker='|')
     plt.show()
 
 if __name__ == '__main__':
--- src/experiments/read_roc_encoding.c
+++ src/experiments/read_roc_encoding.c
@@ -2,7 +2,7 @@
 
 
 int main() {
-    Experiment* exp = experiment_create(4, 723104, "D:\\repos\\Tango_work\\fixing_plots");
+    Experiment* exp = experiment_create(4, 723104, "D:\\repos\\Tango\\outputs\\127-catch-up-with-current-state");
 
     Memory* memory = exp->permanent_memory;
 
@@ -55,14 +55,10 @@ int main() {
 
     network_show(net);
 
-    Callback* cb_meta = callback_meta_dumper_create(
-        memory, exp->output_folder, net);
-    Callback* cb_spikes = callback_spikes_dumper_create(
-        memory, exp->output_folder, net);
-    Callback* cb_weights = callback_weights_dumper_create(
-        memory, 1, 100, exp->output_folder, net);
-    Callback* cb_rescale = callback_synaptic_rescale_create(
-        memory, net, 20000); // add 10000 because we added inhi
+    Callback* cb_meta = callback_meta_dumper_create(memory, exp->output_folder, net);
+    Callback* cb_spikes = callback_spikes_dumper_create(memory, exp->output_folder, net);
+    Callback* cb_weights = callback_weights_dumper_create(memory, 1, 100, exp->output_folder, net);
+    Callback* cb_rescale = callback_synaptic_rescale_create(memory, net, 20000); // add 10000 because we added inhi
     Callback* cb_data = callback_network_data_dumper_create(memory, exp->output_folder, net);
 
     experiment_set_network(exp, net);
